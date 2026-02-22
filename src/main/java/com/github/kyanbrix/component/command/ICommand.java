61c970d7e0ca2eb9dcede5bcba94d3ff6d4efd59
Kian <conagkyanbrix@gamil.com>
2026-02-23 00:10:53 +0800

    added aliases

--- src/main/java/com/github/kyanbrix/component/command/ICommand.java
+++ src/main/java/com/github/kyanbrix/component/command/ICommand.java
@@ -2,11 +2,18 @@ package com.github.kyanbrix.component.command;
 
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface ICommand {
 
     void accept(MessageReceivedEvent event);
 
     String commandName();
+
+    default String[] aliases() {
+        return new String[0];
+    }
 
     default void deleteMessage(MessageReceivedEvent event) {
         event.getMessage().delete().queue();
