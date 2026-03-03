7dfae0e949e8761ed4b06cb9e95f4d750db86d29
kyanbrix <kyanbrix@yahoo.com>
2026-03-03 15:06:26 +0800

    add a channel status event

--- src/main/java/com/github/kyanbrix/features/ServerVoiceLogs.java
+++ src/main/java/com/github/kyanbrix/features/ServerVoiceLogs.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.unions.AudioChannelUnion;
+import net.dv8tion.jda.api.events.channel.update.ChannelUpdateVoiceStatusEvent;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceStreamEvent;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -42,6 +43,30 @@ public class ServerVoiceLogs extends ListenerAdapter {
     }
 
     @Override
+    public void onChannelUpdateVoiceStatus(@NotNull ChannelUpdateVoiceStatusEvent event) {
+
+        Guild guild = event.getGuild();
+
+        TextChannel logChannel = guild.getTextChannelById(1477921214272897125L);
+
+        String status = event.getChannel().asVoiceChannel().getStatus();
+        if (logChannel != null) {
+
+            MessageEmbed embed = new EmbedBuilder()
+                    .setColor(Color.CYAN)
+                    .setDescription(String.format("%s (%s) channel change its status to %s",event.getChannel().getAsMention(),event.getChannel().getName(),status))
+                    .setTimestamp(Instant.now())
+                    .build();
+
+            logChannel.sendMessageEmbeds(embed).queue();
+        }
+
+
+
+
+    }
+
+    @Override
     public void onGuildVoiceUpdate(@NotNull GuildVoiceUpdateEvent event) {
 
         AudioChannelUnion joinedChannel = event.getChannelJoined();
@@ -59,7 +84,7 @@ public class ServerVoiceLogs extends ListenerAdapter {
             builder.setTimestamp(Instant.now());
 
             if (joinedChannel != null && leftChannel != null) {
-                builder.setDescription(String.format("%s moved to %s (%s)",user.getAsMention(),joinedChannel.getAsMention(),joinedChannel.getName()));
+                builder.setDescription(String.format("%s left from %s (%s) and moved to %s (%s)",user.getAsMention(),leftChannel.getAsMention(),leftChannel.getName(),joinedChannel.getAsMention(),joinedChannel.getName()));
                 builder.setColor(Color.blue);
             }else if (joinedChannel != null) {
                 builder.setDescription(String.format("%s has joined voice channel %s (%s)",user.getAsMention(),joinedChannel.getAsMention(),joinedChannel.getName()));
@@ -67,7 +92,7 @@ public class ServerVoiceLogs extends ListenerAdapter {
 
             }else if (leftChannel != null) {
                 builder.setDescription(String.format("%s left voice channel %s (%s) ",user.getAsMention(),leftChannel.getAsMention(),leftChannel.getName()));
-                builder.setColor(Color.orange);
+                builder.setColor(Color.decode("#FF4500"));
 
             }
 
