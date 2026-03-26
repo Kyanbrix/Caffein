8fdac51d56572eff3742b2c3618517fc4d0a8db5
kyanbrix <kyanbrix@yahoo.com>
2026-03-26 13:13:26 +0800

    added self bot status with a scheduler every 1 min

--- src/main/java/com/github/kyanbrix/Caffein.java
+++ src/main/java/com/github/kyanbrix/Caffein.java
@@ -3,7 +3,6 @@ package com.github.kyanbrix;
 import com.github.kyanbrix.component.ConfessionModal;
 import com.github.kyanbrix.component.RoleCreationModal;
 import com.github.kyanbrix.component.StringSelectionComponent;
-import com.github.kyanbrix.component.VerifyModal;
 import com.github.kyanbrix.component.command.CommandManager;
 import com.github.kyanbrix.component.slashcommand.CheckUserLevel;
 import com.github.kyanbrix.component.slashcommand.LeaderboardProfile;
@@ -12,33 +11,51 @@ import com.github.kyanbrix.database.ConnectionPool;
 import com.github.kyanbrix.features.*;
 import com.github.kyanbrix.features.leveling.ChatLeveling;
 import com.github.kyanbrix.features.leveling.VoiceLeveling;
+import com.github.kyanbrix.utils.Constant;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.ChunkingFilter;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
+import org.jspecify.annotations.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-public class Caffein {
+public class Caffein extends ListenerAdapter {
 
     private static final Caffein INSTANCE = new Caffein();
     private static final Logger log = LoggerFactory.getLogger(Caffein.class);
     private JDA jda;
     private ConnectionPool connectionPool;
     private final ScheduledExecutorService service = Executors.newScheduledThreadPool(Runtime.getRuntime().availableProcessors());
-
+    private final ExecutorService executorService = Executors.newFixedThreadPool(3);
     public JDA getJda() {
         return jda;
     }
 
-    private Caffein() {
+    private Caffein() {}
+
+    public ScheduledExecutorService getService() {
+        return service;
     }
+
+    public ExecutorService getExecutorService() {
+        return executorService;
+    }
+
 
     public Connection getConnection() {
         if (connectionPool == null) {
@@ -92,19 +109,50 @@ public class Caffein {
                         GatewayIntent.GUILD_VOICE_STATES,
                         GatewayIntent.DIRECT_MESSAGES,
                         GatewayIntent.GUILD_EXPRESSIONS,
-                GatewayIntent.SCHEDULED_EVENTS
+                        GatewayIntent.SCHEDULED_EVENTS,
+                        GatewayIntent.GUILD_MODERATION
                 )
                 .enableCache(CacheFlag.VOICE_STATE, CacheFlag.EMOJI, CacheFlag.SCHEDULED_EVENTS,CacheFlag.STICKER)
-                .setMemberCachePolicy(member -> member.getGuild().getIdLong() == 1469324454470353163L)
+                .setMemberCachePolicy(member -> member.getGuild().getIdLong() == Constant.SERVER_CAFE_ID)
                 .setChunkingFilter(ChunkingFilter.ALL)
-                .addEventListeners(new VoiceLeveling(),new ChatLeveling(),new CommandManager(), new StringSelectionComponent(), new ButtonManager(), new ServerMemberHandler(), new InviteTracker(), new Assistant(), new ServerVoiceLogs(), new ConfessionModal(), new RoleCreationModal(), new VerifyModal())
-                .addEventListeners(slashmanager)
+                .addEventListeners(new VoiceLeveling(),new ChatLeveling(),new CommandManager(), new StringSelectionComponent(), new ButtonManager(), new ServerMemberHandler(), new InviteTracker(), new Assistant(), new ServerVoiceLogs(), new RoleCreationModal(), new ConfessionModal(), new ServerAuditLogsListener())
+                .addEventListeners(slashmanager, INSTANCE)
                 .setEnableShutdownHook(false)
                 .build().awaitReady();
 
 
+
+
+
+
     }
 
+    @Override
+    public void onReady(@NonNull ReadyEvent event) {
 
+        log.info("Bot is Ready!");
+
+        JDA bot = event.getJDA();
+
+
+        service.scheduleAtFixedRate(() -> {
+
+            Guild guild = bot.getGuildById(Constant.SERVER_CAFE_ID);
+
+            if (guild == null) return;
+
+            List<Member> members = guild.getMembers().stream().filter(member -> !member.getUser().isBot()).toList();
+
+            if (members.isEmpty()) return;
+
+            bot.getPresence().setActivity(Activity.watching(String.format("%d members",members.size())));
+
+        },0,1, TimeUnit.MINUTES);
+
+
+
+
+
+    }
 
 }
