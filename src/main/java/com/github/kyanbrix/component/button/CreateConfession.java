423033887524dc1f4c7aba62a703f109b84a2ee6
kyanbrix <kyanbrix@yahoo.com>
2026-03-26 13:12:06 +0800

    Remove attachment option

--- src/main/java/com/github/kyanbrix/component/button/CreateConfession.java
+++ src/main/java/com/github/kyanbrix/component/button/CreateConfession.java
@@ -1,6 +1,5 @@
 package com.github.kyanbrix.component.button;
 
-import net.dv8tion.jda.api.components.attachmentupload.AttachmentUpload;
 import net.dv8tion.jda.api.components.label.Label;
 import net.dv8tion.jda.api.components.textinput.TextInput;
 import net.dv8tion.jda.api.components.textinput.TextInputStyle;
@@ -16,10 +15,8 @@ public class CreateConfession implements IButton{
 
         Modal modal = Modal.create("confession","My Confession")
                 .addComponents(
-                        Label.of("Confession", TextInput.create("confess", TextInputStyle.PARAGRAPH).setMinLength(3).setRequired(true).build()),
-                        Label.of("Attachment (Optional)", AttachmentUpload.create("attachment-upload").setRequired(false).build())
+                        Label.of("Confession", TextInput.create("confess", TextInputStyle.PARAGRAPH).setMaxLength(300).setRequired(true).build())
                 ).build();
-
 
         event.replyModal(modal).queue();
 
