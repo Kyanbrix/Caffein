ebccd96a00d897ce2e4e55d76b1d140dfac19d63
kyanbrix <kyanbrix@yahoo.com>
2026-09-12 01:57:27 +0800

    just converted into record nothing else

--- src/main/java/com/github/kyanbrix/features/data/Data.java
+++ src/main/java/com/github/kyanbrix/features/data/Data.java
@@ -1,22 +1,10 @@
-package com.github.kyanbrix.features.leveling.utilities;
+package com.github.kyanbrix.features.data;
 
-public class Data {
-    public final String username;
-    public final int    level;
-    public final int    xpProgress;   // XP within current level
-    public final int    xpNeeded;     // XP needed to reach next level
-    public final int    rank;
-    public final String avatarUrl;
-
-    public Data(String username, int level, int xpProgress,
-                int xpNeeded, int rank, String avatarUrl) {
-        this.username    = username;
-        this.level       = level;
-        this.xpProgress  = xpProgress;
-        this.xpNeeded    = xpNeeded;
-        this.rank        = rank;
-        this.avatarUrl   = avatarUrl;
-    }
+/**
+ * @param xpProgress XP within current level
+ * @param xpNeeded   XP needed to reach next level
+ */
+public record Data(String username, int level, int xpProgress, int xpNeeded, int rank, String avatarUrl) {
 
 
 }
